d8a2aa58a2ea4646bfa7aa2e320f3619a83d322f
alexkariuki <alexkariuki2@gmail.com>
2021-07-23 12:13:45 +0300

    Added video to home page and basic styling done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,26 @@ const routes: Routes = [
   {
     path: '',
     loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+  },
+  {
+    path: 'home',
+    loadChildren: () => import('./pages/home/home.module').then( m => m.HomePageModule)
+  },
+  {
+    path: 'discover',
+    loadChildren: () => import('./pages/discover/discover.module').then( m => m.DiscoverPageModule)
+  },
+  {
+    path: 'inbox',
+    loadChildren: () => import('./pages/inbox/inbox.module').then( m => m.InboxPageModule)
+  },
+  {
+    path: 'me',
+    loadChildren: () => import('./pages/me/me.module').then( m => m.MePageModule)
+  },
+  {
+    path: 'upload',
+    loadChildren: () => import('./pages/upload/upload.module').then( m => m.UploadPageModule)
   }
 ];
 @NgModule({
--- src/app/tabs/tabs-routing.module.ts
+++ src/app/tabs/tabs-routing.module.ts
@@ -9,16 +9,25 @@ const routes: Routes = [
     children: [
       {
         path: 'tab1',
-        loadChildren: () => import('../tab1/tab1.module').then(m => m.Tab1PageModule)
+        loadChildren: () => import('../pages/home/home.module').then(m => m.HomePageModule)
       },
       {
         path: 'tab2',
-        loadChildren: () => import('../tab2/tab2.module').then(m => m.Tab2PageModule)
+        loadChildren: () => import('../pages/discover/discover.module').then(m => m.DiscoverPageModule)
       },
       {
         path: 'tab3',
-        loadChildren: () => import('../tab3/tab3.module').then(m => m.Tab3PageModule)
+        loadChildren: () => import('../pages/upload/upload.module').then(m => m.UploadPageModule)
       },
+      {
+        path: 'tab4',
+        loadChildren: () => import('../pages/inbox/inbox.module').then(m => m.InboxPageModule)
+      },
+      {
+        path: 'tab5',
+        loadChildren: () => import('../pages/me/me.module').then(m => m.MePageModule)
+      },
+
       {
         path: '',
         redirectTo: '/tabs/tab1',
